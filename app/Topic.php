f4eccf407c1d9647d3148a81dee570632ce5425d
Gardian <gardianii@hotmail.com>
2020-12-25 20:18:17 +0100

    Relationships Topic and Post

--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    protected $fillable = ['title'];
+    //Topic belongs to user and have many post 
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
